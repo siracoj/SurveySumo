339b90116b114380d0bfb1a2818a6841c24f9539
siracoj <siraco.jared@gmail.com>
2017-01-14 20:34:59 -0500

    Added question and answer models

--- survey_sumo/survey/models.py
+++ survey_sumo/survey/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    """
+    Model to hold all questions to be answered by users
+    """
+    question = models.CharField(max_length=1000)
+
+
+class Answer(models.Model):
+    """
+    Model to hold users answers
+    Contains which user answered, what question they answered, and the answer itself
+    """
+
+    user = models.ForeignKey(User)
+    question = models.ForeignKey(Question)
+    answer = models.CharField(max_length=1000)
+
+
+
+
