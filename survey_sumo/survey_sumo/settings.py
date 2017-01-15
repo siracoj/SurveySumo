a5d02194e9e5fc8407af0e0402094967c8f25fbc
siracoj <siraco.jared@gmail.com>
2017-01-14 20:10:37 -0500

    MySQL configuration complete

--- survey_sumo/survey_sumo/settings.py
+++ survey_sumo/survey_sumo/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'survey_sumo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'survey_sumo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
